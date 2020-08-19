cc5bc30d2cc4178ac2f81aba323a9e87dda98132
daniel-lucas-silva <dluc18@gmail.com>
2020-08-19 10:35:09 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-b5gmd8-zrczkz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-b5gmd8-zrczkz)
---
+++ src/app/swipe-info.interface.ts
@@ -0,0 +1,8 @@
+export interface SwipeInfo{
+    x1: number;
+    y1: number;
+    x2: number;
+    y2: number;
+    scrolling: boolean | null;
+    manual: boolean;
+  }
